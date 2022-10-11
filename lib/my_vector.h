4d21a6a05bae11874f41fbfe97a284b39e240747
pxlhack <nellsouhd@gmail.com>
2022-10-11 20:46:29 +0700

    created-myvector-class

---
+++ lib/my_vector.h
@@ -0,0 +1,11 @@
+
+#ifndef MY_VECTOR_MY_VECTOR_H
+#define MY_VECTOR_MY_VECTOR_H
+
+
+class MyVector {
+
+};
+
+
+#endif //MY_VECTOR_MY_VECTOR_H
---
+++ src/my_vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pxlhack on 11.10.22.
+//
+
+#include "../lib/my_vector.h"
